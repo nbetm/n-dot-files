c6dfa9fe8594b65f28d9c20c24944926509e4b44
Nelson R Monserrate <MonserrateNelson@JohnDeere.com>
2015-08-12 13:14:06 -0500

    Several changes to Vi TERM/colors, tmux & irssi
    * Reordered color section in vimrc so colorschemes installed with
      Vundle are able to load at runtime
    * Improved terminal detection inside vimrc
    * Switched to solarized colorscheme
    * Switched AirlineTheme to base16 and match Tmux status line
    * Disabled pane-border-fg/bg on tmux.conf
    * Renamed irssi func wrapper to irssi_nicklist()
    * Inverted panes order on irssi_nicklist()
    * Changed default-terminal to xterm on Tmux

--- bash/plugins/irssi.plugins.bash
+++ bash/plugins/irssi.plugins.bash
@@ -1,22 +1,24 @@
 #
-# Function: irssi()
+# Function: irssi_nicklist()
 #
 # Irssi wrapper for tmux. It will split window #0 in two vertical panes:
-# Left  -> IRC main console
-# Right -> Nicklist using FIFO file
+#   Right -> Nicklist using FIFO file
+#   Left  -> IRC main console
 #
-irssi() {
+irssi_nicklist() {
   # bypass tmux config if not inside tmux
   if [[ ! -z $TMUX ]]; then
     # prepare tmux window
-    $TMUX_BIN rename-window irc                                     # name the window
-    $TMUX_BIN -q set-window main-pane-width $(($(tput cols) - 20))  # set the main pane width to the total width - 20
-    $TMUX_BIN split-window -v "cat ~/.irssi/nicklistfifo"           # create the window and begin reading the fifo
-    $TMUX_BIN -q select-layout main-vertical                        # assign the layout
-    $TMUX_BIN select-window -t irc                                  # select window
-    $TMUX_BIN select-pane -t 0                                      # select pane 0
+    $TMUX_BIN rename-window irc                             # name the window
+    $TMUX_BIN -q set-window main-pane-width 20              # set the main pane width to the total width - 20
+    $TMUX_BIN split-window -v "cat ~/.irssi/nicklistfifo"   # create the window and begin reading the fifo
+    $TMUX_BIN -q select-layout main-vertical                # assign the layout
+    $TMUX_BIN select-window -t irc                          # select window
+    $TMUX_BIN swap-pane -U                                  # swap-panes
+    $TMUX_BIN select-pane -t 1                              # select pane 1
   fi
 
   # execute irssi
-  TERM=screen-256color $IRSSI_BIN $@
+  # TERM=screen-256color $IRSSI_BIN $@
+  $IRSSI_BIN $@
 }
