fc2728291af52a343f2bf9cbe5b8888e082a0f32
Nelson R Monserrate <nrmonserrate@gmail.com>
2015-12-03 13:01:02 -0600

    Added git aliases: gtree & gfetch

--- bash/aliases/git.aliases.bash
+++ bash/aliases/git.aliases.bash
@@ -1,2 +1,4 @@
 # Git
 alias g='git'
+alias gtree='git log --all --decorate --max-count=20 --abbrev-commit --graph --pretty=format:"%C(yellow)%h%Creset %C(cyan)%C(bold)%an%Creset %C(green)%cr%Creset %C(magenta)%d%Creset %C(white)%s"'
+alias gfetch='git fetch --all --prune --tags'
