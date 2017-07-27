b1f9c111197b27822ee2c1322b4357ec69127b96
Nelson R Monserrate <nrmonserrate@gmail.com>
2017-07-26 23:53:23 -0400

    Add fzf plugin into bash plugins section

---
+++ roles/bash/files/plugins/fzf.plugins.bash
@@ -0,0 +1,45 @@
+#
+# MIT License
+#
+# Copyright (c) 2017 Nelson R Monserrate
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# ------------------------------------------------------------------------------
+# FZF Color Scheme
+# ------------------------------------------------------------------------------
+
+# Dark (default)
+# export FZF_DEFAULT_OPTS='--color dark'
+
+# Light
+# export FZF_DEFAULT_OPTS='--color light'
+
+# Solarized Dark
+# export FZF_DEFAULT_OPTS='
+  # --color dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254
+  # --color info:254,prompt:37,spinner:108,pointer:235,marker:235
+# '
+
+# Solarized Light
+# export FZF_DEFAULT_OPTS='
+  # --color fg:240,bg:230,hl:33,fg+:241,bg+:221,hl+:33
+  # --color info:33,prompt:33,pointer:166,marker:166,spinner:33
+# '
